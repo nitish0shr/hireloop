665562d7067de40821af77470a4730317dfd0b0c
nitish0shr <nitish0shr@gmail.com>
2025-10-25 18:55:28 -0500

    Add candidates page

---
+++ apps/web/app/candidates/page.tsx
@@ -0,0 +1,8 @@
+export default function CandidatesPage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Candidates</h1>
+      <p>Coming Soon</p>
+    </div>
+  );
+}
